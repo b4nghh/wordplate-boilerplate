5c406fe5e41588cd2df2f1b3c7b9de76ee49e746
Bang Hoang <bang.hh@neo-lab.vn>
2020-05-17 22:34:14 +0700

    Add Soil plugin

--- public/themes/wordplate/app/filters.php
+++ public/themes/wordplate/app/filters.php
@@ -1,1 +1,10 @@
 <?php
+
+/**
+ * Add "… Continued" to the excerpt.
+ *
+ * @return string
+ */
+add_filter('excerpt_more', function () {
+    return ' &hellip; <a href="' . get_permalink() . '">' . __('Continued', 'sage') . '</a>';
+});
--- public/themes/wordplate/app/setup.php
+++ public/themes/wordplate/app/setup.php
@@ -10,3 +10,80 @@ add_action('wp_enqueue_scripts', function () {
 
     wp_enqueue_script('wordplate');
 });
+
+/**
+ * Register the initial theme setup.
+ *
+ * @return void
+ */
+add_action('after_setup_theme', function () {
+    /**
+     * Enable features from Soil when plugin is activated
+     * @link https://roots.io/plugins/soil/
+     */
+    add_theme_support('soil-clean-up');
+    // add_theme_support('soil-disable-rest-api');
+    // add_theme_support('soil-disable-asset-versioning');
+    // add_theme_support('soil-disable-trackbacks');
+    // add_theme_support('soil-google-analytics', 'UA-XXXXX-Y');
+    // add_theme_support('soil-js-to-footer');
+    // add_theme_support('soil-nav-walker');
+    add_theme_support('soil-nice-search');
+    add_theme_support('soil-relative-urls');
+
+    /**
+     * Enable plugins to manage the document title
+     * @link https://developer.wordpress.org/reference/functions/add_theme_support/#title-tag
+     */
+    add_theme_support('title-tag');
+
+    /**
+     * Register navigation menus
+     * @link https://developer.wordpress.org/reference/functions/register_nav_menus/
+     */
+    register_nav_menus([
+        'primary_navigation' => __('Primary Navigation', 'wordplate')
+    ]);
+
+    /**
+     * Enable post thumbnails
+     * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+     */
+    add_theme_support('post-thumbnails');
+
+    /**
+     * Add theme support for Wide Alignment
+     * @link https://wordpress.org/gutenberg/handbook/designers-developers/developers/themes/theme-support/#wide-alignment
+     */
+    add_theme_support('align-wide');
+
+    /**
+     * Enable responsive embeds
+     * @link https://wordpress.org/gutenberg/handbook/designers-developers/developers/themes/theme-support/#responsive-embedded-content
+     */
+    add_theme_support('responsive-embeds');
+
+    /**
+     * Enable HTML5 markup support
+     * @link https://developer.wordpress.org/reference/functions/add_theme_support/#html5
+     */
+    add_theme_support('html5', ['caption', 'comment-form', 'comment-list', 'gallery', 'search-form']);
+
+    /**
+     * Enable selective refresh for widgets in customizer
+     * @link https://developer.wordpress.org/themes/advanced-topics/customizer-api/#theme-support-in-sidebars
+     */
+    add_theme_support('customize-selective-refresh-widgets');
+
+    /**
+     * Enable theme color palette support
+     * @link https://developer.wordpress.org/block-editor/developers/themes/theme-support/#block-color-palettes
+     */
+    add_theme_support('editor-color-palette', [
+        [
+            'name'  => __('Primary', 'wordplate'),
+            'slug'  => 'primary',
+            'color' => '#525ddc',
+        ]
+    ]);
+}, 20);
